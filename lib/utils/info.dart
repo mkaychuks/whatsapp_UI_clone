71b7545a68f83c049b64b8bc77f2e3848a090694
mkaychuks <sachufusi@gmail.com>
2022-12-02 13:39:53 +0100

    Minor changes to the dummy data

--- lib/utils/info.dart
+++ lib/utils/info.dart
@@ -141,7 +141,7 @@ const calls = [
     'time': 'Yesterday 10:05 am',
   },
   {
-    'name': 'viktor',
+    'name': 'Viktor',
     'profilePic':
         'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     'time': 'Yesterday 3:53 pm',
