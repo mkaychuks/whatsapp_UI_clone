191cc018b027129f737981197daac02a33d8bc5a
mkaychuks <sachufusi@gmail.com>
2022-12-02 13:40:10 +0100

    Built the basic UI of the status screen

--- lib/screens/status_screen.dart
+++ lib/screens/status_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui_clone/utils/colors.dart';
+import 'package:whatsapp_ui_clone/utils/info.dart';
 
 class StatusScreenPage extends StatelessWidget {
   const StatusScreenPage({super.key});
@@ -18,7 +19,9 @@ class StatusScreenPage extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          const SizedBox(height: 10,),
+          const SizedBox(
+            height: 10,
+          ),
           FloatingActionButton(
             backgroundColor: appBarColor,
             onPressed: () {},
@@ -29,6 +32,102 @@ class StatusScreenPage extends StatelessWidget {
           ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8.0, left: 10.0, right: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              leading: aviStatus(),
+              title: const Text('My Status'),
+              subtitle: const Text('Tap to add status update'),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            const Text(
+              "Recent updates",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: calls.length,
+                itemBuilder: (context, index) {
+                  return statusListItem(
+                    calls[index]['name'],
+                    calls[index]['time'],
+                    calls[index]['profilePic'],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+// the stack is the stack for the CircularAvatar at the top of the screen
+  Stack aviStatus() {
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [mainContainer(), childContainer()],
+    );
+  }
+
+// the parent container that has the child on the stack
+  Container mainContainer() {
+    return Container(
+      width: 50,
+      height: 50,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+              ),
+        ),
+      ),
+    );
+  }
+
+// the child container of the stack in question, refer to the above...
+  Container childContainer() {
+    return Container(
+      width: 25,
+      height: 25,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.green,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.white, width: 2)),
+      child: const Text(
+        '+',
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+
+  ListTile statusListItem(
+      String? title, String? subtitle, String? networkLink) {
+    return ListTile(
+      leading: Container(
+        width: 50,
+        height: 50,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(image: NetworkImage(networkLink!))),
+      ),
+      title: Text(title!),
+      subtitle: Text(subtitle!),
     );
   }
 }
